d90034ba2806eb5f6d12f79c1f4f70a35a320d2c
tommywwz <wangweizhe2001@gmail.com>
2021-11-21 17:22:54 -0500

    add buzzer routine

--- ECE-2799.ino
+++ ECE-2799.ino
@@ -7,6 +7,8 @@ const int ANALOGPIN=0;
 MQ135 gasSensor = MQ135(ANALOGPIN);
 
 unsigned int counter = 0;
+unsigned int i = 0;
+int decre = 0; // 0--increament, 1--decreament
 float ppm;
 
 void setup(){
@@ -25,18 +27,18 @@ void setup(){
     // enable timer compare interrupt
     TIMSK0 |= (1 << OCIE0A);
 
-  // //set timer1 interrupt at 1Hz
-  //   TCCR1A = 0;// set entire TCCR1A register to 0
-  //   TCCR1B = 0;// same for TCCR1B
-  //   TCNT1  = 0;//initialize counter value to 0
-  //   // set compare match register for 1hz increments
-  //   OCR1A = 15624;// = (16*10^6) / (1*1024) - 1 (must be <65536)
-  //   // turn on CTC mode
-  //   TCCR1B |= (1 << WGM12);
-  //   // Set CS10 and CS12 bits for 1024 prescaler
-  //   TCCR1B |= (1 << CS12) | (1 << CS10);  
-  //   // enable timer compare interrupt
-  //   TIMSK1 |= (1 << OCIE1A);
+  //set timer1 interrupt at 1Hz
+    TCCR1A = 0;// set entire TCCR1A register to 0
+    TCCR1B = 0;// same for TCCR1B
+    TCNT1  = 0;//initialize counter value to 0
+    // set compare match register for 1hz increments
+    OCR1A = 155;// = (16*10^6) / (1*1024) - 1 (must be <65536)
+    // turn on CTC mode
+    TCCR1B |= (1 << WGM12);
+    // Set CS10 and CS12 bits for 1024 prescaler
+    TCCR1B |= (1 << CS12) | (1 << CS10);  
+    // enable timer compare interrupt
+    TIMSK1 |= (1 << OCIE1A);
 
   // //set timer2 interrupt at 8kHz
   //   TCCR2A = 0;// set entire TCCR2A register to 0
@@ -68,21 +70,25 @@ ISR(TIMER0_COMPA_vect){//timer0 interrupt 1kHz
   }
 }
 
+ISR(TIMER1_COMPA_vect){//timer0 interrupt 1kHz
+  if (i == 255) {
+    decre = 1;
+  } else if (i == 0) {
+    decre = 0;
+  }
+  if (decre) { // if decreament
+    i --;
+  } else {
+    i ++;
+  }
+  if (ppm > 300)  
+    analogWrite(buzzer, i); //raise the voltage sent out of the pin by 1
+  else
+    analogWrite(buzzer, 0); //stop the buzzer
+}
+
 void loop(){
 
-  if (ppm > 500) {
-    for(int i=0; i<255; i++) { //do this 255 times
-      analogWrite(buzzer, i); //raise the voltage sent out of the pin by 1
-      delay(10); //wait 10 milliseconds to repeat 
-    }
-
-    for(int i=255; i>0; i--) { // do this 255 times
-      analogWrite(buzzer, i); //lower the voltage sent out of the pin by 1
-      delay(10); //wait 10 milliseconds to repeat
-    }
-  } else {
-    analogWrite(buzzer, 0); //raise the voltage sent out of the pin by 1
-  }
 }
 // words in the double slash are not part of the code
 
