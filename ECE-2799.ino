2a22ead4bb592ee078d6277d53c45199efb7d041
tommywwz <wangweizhe2001@gmail.com>
2021-11-23 17:11:42 -0500

    add data averaging

--- ECE-2799.ino
+++ ECE-2799.ino
@@ -1,6 +1,8 @@
 #include "MQ135.h"
 #include <avr/interrupt.h>
 #define buzzer 11 //change to any output pin (not analog inputs) 
+#define PPM_BUFFER_SIZE 5 // size - 1
+#define PPM_THRSHD 300
 
 const int ANALOGPIN=0;
 
@@ -9,7 +11,11 @@ MQ135 gasSensor = MQ135(ANALOGPIN);
 unsigned int counter = 0;
 unsigned int i = 0;
 int decre = 0; // 0--increament, 1--decreament
+
 float ppm;
+unsigned index = 0;
+float sumppm = 0.0f;
+float ppm_buff [PPM_BUFFER_SIZE];
 
 void setup(){
   cli();//stop interrupts
@@ -18,8 +24,8 @@ void setup(){
     TCCR0A = 0;// set entire TCCR0A register to 0
     TCCR0B = 0;// same for TCCR0B
     TCNT0  = 0;//initialize counter value to 0
-    // set compare match register for 2khz increments
-    OCR0A = 249;// = (16*10^6) / (2000*64) - 1 (must be <256)
+    // set compare match register for 1khz increments
+    OCR0A = 249;// = (16*10^6) / (1000*64) - 1 (must be <256)
     // turn on CTC mode
     TCCR0A |= (1 << WGM01);
     // Set CS01 and CS00 bits for 64 prescaler
@@ -32,7 +38,7 @@ void setup(){
     TCCR1B = 0;// same for TCCR1B
     TCNT1  = 0;//initialize counter value to 0
     // set compare match register for 1hz increments
-    OCR1A = 155;// = (16*10^6) / (1*1024) - 1 (must be <65536)
+    OCR1A = 15624;// = (16*10^6) / (1*1024) - 1 (must be <65536)
     // turn on CTC mode
     TCCR1B |= (1 << WGM12);
     // Set CS10 and CS12 bits for 1024 prescaler
@@ -58,38 +64,60 @@ void setup(){
 
   Serial.begin(9600);
   pinMode(buzzer, OUTPUT); //tell arduino the buzzer is an output device
+
+  // load ppm_buffer
+  for (int i = 0; i < PPM_BUFFER_SIZE; i++) {
+    ppm_buff [i] = 0.0f;
+  }
 }
 
-ISR(TIMER0_COMPA_vect){//timer0 interrupt 1kHz
-  if (counter > 999) {
+ISR(TIMER1_COMPA_vect){//timer1 interrupt 1Hz
+  char readings [500];
+  ppm = gasSensor.getPPM();
+  Serial.println(ppm);
+
+  if (index < PPM_BUFFER_SIZE - 1) {
+    sumppm += ppm;
+    sumppm = sumppm - ppm_buff[index];
+    ppm_buff[index] = ppm;
+    index ++;
+  } else {
+    sumppm += ppm;
+    sumppm = sumppm - ppm_buff[index];
+    ppm_buff[index] = ppm;
+    index = 0;
+  }
+
+  // Serial.println(sumppm);
+}
+
+ISR(TIMER0_COMPA_vect){//timer1 interrupt at 1kHz
+  if (counter > 9) {
     counter = 0;
-    ppm = gasSensor.getPPM();
-    Serial.println(ppm);
+    if (i == 255) {
+      decre = 1;
+    } else if (i == 0) {
+      decre = 0;
+    }
+    if (decre) { // if decreament
+      i --;
+    } else {
+      i ++;
+    }
+    if (sumppm > PPM_BUFFER_SIZE * PPM_THRSHD)  
+      analogWrite(buzzer, i); //raise the voltage sent out of the pin by 1
+    else
+      analogWrite(buzzer, 0); //stop the buzzer
   } else {
     counter ++;
   }
 }
 
-ISR(TIMER1_COMPA_vect){//timer0 interrupt 1kHz
-  if (i == 255) {
-    decre = 1;
-  } else if (i == 0) {
-    decre = 0;
-  }
-  if (decre) { // if decreament
-    i --;
-  } else {
-    i ++;
-  }
-  if (ppm > 300)  
-    analogWrite(buzzer, i); //raise the voltage sent out of the pin by 1
-  else
-    analogWrite(buzzer, 0); //stop the buzzer
-}
-
 void loop(){
 
 }
+
+
 // words in the double slash are not part of the code
 
 // #define buzzer 11 //change to any output pin (not analog inputs) 
